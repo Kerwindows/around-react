6437203b67900302b0e5a5419b6e97e5f9a0c3a9
Kerwindows <kerwindows@hotmail.com>
2022-09-04 19:36:42 -0400

    hotfix for default api

--- src/utils/api.js
+++ src/utils/api.js
@@ -79,10 +79,12 @@ class Api {
   }
 }
 
-export default const api = new Api({
+const api = new Api({
   baseUrl: "https://around.nomoreparties.co/v1/group-12",
   headers: {
     authorization: "388e1377-9ab5-4db7-9c4c-d98eb5bc0391",
     "Content-Type": "application/json"
   }
 });
+
+export default api;
